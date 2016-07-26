b576c5c9d2d14153dc3e39a3028e0fb3078a4987
Jeremy <jkaffrey@gmail.com>
2016-07-26 09:36:10 -0600

    Robot can now function

--- robot.js
+++ robot.js
@@ -1,7 +1,65 @@
+'use strict';
+
 var Robot = function (startCharge) {
+
+  this.charge = startCharge;
+  this.myLoc = [0,0];
+  this.hasDone = [];
+
+  this.battery = function() {
+
+    return this.charge;
+  };
+
+  this.moveUp = function() {
+
+    if (this.charge > 0) {
+
+      this.myLoc[1]++;
+      this.hasDone.push('up');
+      this.charge--;
+    }
+  };
+
+  this.moveDown = function() {
+
+    if (this.charge > 0) {
+
+      this.myLoc[1]--;
+      this.hasDone.push('down');
+      this.charge--;
+    }
+  };
+
+  this.moveLeft = function() {
+
+    if (this.charge > 0) {
+
+      this.myLoc[0]--;
+      this.hasDone.push('left');
+      this.charge--;
+    }
+  };
+
+  this.moveRight = function() {
+
+    if (this.charge > 0) {
+
+      this.myLoc[0]++;
+      this.hasDone.push('right');
+      this.charge--;
+    }
+  };
+
+  this.history = function() {
+
+    return this.hasDone;
+  };
 };
 
 Robot.prototype.position = function() {
+
+  return this.myLoc;
 };
 
 module.exports = Robot;
