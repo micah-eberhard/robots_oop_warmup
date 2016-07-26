ca8080fa98c5c84695a5680e29b8a2ace6c4f594
Jamie <lanae_j@operamail.com>
2016-07-26 09:50:02 -0600

    robot done

--- robot.js
+++ robot.js
@@ -1,7 +1,38 @@
-var Robot = function (startCharge) {
+'use strict';
+var Robot = function(startCharge) {
+    this.startCharge = startCharge;
+    this.positioncoord = [0, 0];
+    this.histarr = [];
+};
+Robot.prototype.moveUp = function() {
+    if (this.startCharge > 0) {
+        this.positioncoord[1]++;
+        this.histarr.push("up");
+        this.startCharge--;
+    }
+};
+Robot.prototype.moveLeft = function() {
+    if (this.startCharge > 0) {
+        this.positioncoord[0]--;
+        this.histarr.push("left");
+        this.startCharge--;
+
+    }
+};
+Robot.prototype.history = function() {
+    return this.histarr;
+};
+Robot.prototype.position = function() {
+
+    return this.positioncoord;
+
 };
 
-Robot.prototype.position = function() {
+Robot.prototype.battery = function() {
+
+    //var level=this.startCharge;
+    return this.startCharge;
 };
+
 
 module.exports = Robot;
