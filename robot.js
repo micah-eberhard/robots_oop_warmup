a4a42e9fd3ce0ef14b4b36459622e9a79b3f2fb9
Michael Mesereau <mmesereau1@gmail.com>
2016-07-26 09:27:55 -0600

    completed exercise

--- robot.js
+++ robot.js
@@ -1,7 +1,34 @@
+'use strict';
+
 var Robot = function (startCharge) {
+  this.charge = startCharge;
+  this.location = [0,0];
+  this.moves = [];
+  this.history = function () {
+    return this.moves;
+  }
+  this.battery = function() {
+    return this.charge;
+  }
+  this.moveUp = function() {
+    if (this.charge > 0) {
+      this.location[1]++;
+      this.moves.push('up');
+      this.charge--;
+    }
+  }
+  this.moveLeft = function() {
+    if (this.charge > 0) {
+      this.location[0]--;
+      this.moves.push('left');
+      this.charge--;
+    }
+  }
 };
 
+
 Robot.prototype.position = function() {
+  return this.location;
 };
 
 module.exports = Robot;
